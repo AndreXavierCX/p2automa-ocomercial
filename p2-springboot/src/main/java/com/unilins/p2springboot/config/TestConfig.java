c9d9cfb0f3c7eb23471540f1a322bec40c01cd50
AndreXavierCX <andrexavier99864@gmail.com>
2023-12-08 23:36:07 -0300

    first commit

--- p2-springboot/src/main/java/com/unilins/p2springboot/config/TestConfig.java
+++ p2-springboot/src/main/java/com/unilins/p2springboot/config/TestConfig.java
@@ -15,34 +15,33 @@ import com.unilins.p2springboot.repositories.*;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 	@Autowired
-	private  ClienteRepository clienteRep;
-	
+	private ClienteRepository clienteRep;
+
 	@Autowired
 	private CarroRepository carroRep;
-	
+
 	@Autowired
-	private AluguelRepository aluguelRepository; 
-	
-	public  void run(String... args) throws Exception{
-		Cliente cliente1 = new Cliente(null,"João Silva", "123.456.789-00", "(11) 98765-4321", "1234567890");
-		Cliente cliente2 = new Cliente(null,"Maria Souza", "987.654.321-00", "(22) 99999-8888", "0987654321");
-		Cliente cliente3 = new Cliente(null,"Carlos Oliveira", "111.222.333-44", "(33) 87654-1234", "5678901234");
-		Cliente cliente4 = new Cliente(null,"Ana Santos", "555.666.777-88", "(44) 11111-2222", "9876543210");
-		Cliente cliente5 = new Cliente(null,"Pedro Ferreira", "999.888.777-66", "(55) 33333-4444", "5432109876");
-		clienteRep.saveAll(Arrays.asList(cliente1,cliente2,cliente3,cliente4,cliente5));
+	private AluguelRepository aluguelRepository;
 
-		Carro carro1 = new Carro(null, "Toyota", "Corolla", "ABC1234", (float) 150.0);
-		Carro carro2 = new Carro(null, "Honda", "Civic", "DEF5678", (float) 170.0);
-		Carro carro3 = new Carro(null, "Chevrolet", "Onix", "GHI9012", (float) 130.0);
-		Carro carro4 = new Carro(null, "Ford", "Focus", "JKL3456", (float) 160.0);
-		Carro carro5 = new Carro(null, "Volkswagen", "Gol", "MNO7890", (float) 140.0);
-		carroRep.saveAll(Arrays.asList(carro1,carro2,carro3,carro4,carro5));
+	public void run(String... args) throws Exception {
+		Cliente cliente1 = new Cliente(null, "José Oliveira", "111.222.333-44", "(11) 98765-4321", "1234567890");
+		Cliente cliente2 = new Cliente(null, "Patrícia Lima", "222.333.444-55", "(22) 99999-8888", "0987654321");
+		Cliente cliente3 = new Cliente(null, "Fernanda Pereira", "333.444.555-66", "(33) 87654-1234", "5678901234");
+		Cliente cliente4 = new Cliente(null, "Ricardo Santos", "444.555.666-77", "(44) 11111-2222", "9876543210");
+		Cliente cliente5 = new Cliente(null, "Mariana Silva", "555.666.777-88", "(55) 33333-4444", "5432109876");
+		clienteRep.saveAll(Arrays.asList(cliente1, cliente2, cliente3, cliente4, cliente5));
 
-		Aluguel aluguel1 = new Aluguel(carro1,cliente1,  "12/07/2023","15/07/2023", (float) 130.5);
-		Aluguel aluguel2 = new Aluguel(carro4,cliente2, "12/08/2023","15/08/2023", (float) 140.5);
-		Aluguel aluguel3 = new Aluguel(carro3, cliente5, "12/09/2023","15/09/2023",(float) 150);
-		aluguelRepository.saveAll(Arrays.asList(aluguel1, aluguel2,aluguel3));
+		Carro carro1 = new Carro(null, "Volkswagen", "Jetta", "XYZ5678", (float) 180.0);
+		Carro carro2 = new Carro(null, "Fiat", "Uno", "UVW1234", (float) 120.0);
+		Carro carro3 = new Carro(null, "Renault", "Sandero", "LMN9012", (float) 135.0);
+		Carro carro4 = new Carro(null, "Hyundai", "HB20", "PQR3456", (float) 155.0);
+		Carro carro5 = new Carro(null, "Kia", "Sportage", "STU7890", (float) 200.0);
+		carroRep.saveAll(Arrays.asList(carro1, carro2, carro3, carro4, carro5));
 
+		Aluguel aluguel1 = new Aluguel(carro1, cliente1, "20/07/2023", "25/07/2023", (float) 160.75);
+		Aluguel aluguel2 = new Aluguel(carro4, cliente2, "20/08/2023", "25/08/2023", (float) 175.0);
+		Aluguel aluguel3 = new Aluguel(carro3, cliente5, "20/09/2023", "25/09/2023", (float) 185.25);
+		aluguelRepository.saveAll(Arrays.asList(aluguel1, aluguel2, aluguel3));
 
 	}
 }
